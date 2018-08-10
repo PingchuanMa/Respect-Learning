68e81454ad9117a40b555c4f345f12d7939e69a2
knoxtian <knoxtian@icloud.com>
2018-08-11 05:35:01 +0800

    bug fix for submission

--- submit.py
+++ submit.py
@@ -2,6 +2,9 @@ import opensim as osim
 from osim.http.client import Client
 from osim.env import ProstheticsEnv
 import argparse
+import multiprocessing
+import tensorflow as tf
+import numpy as np
 
 import os
 import sys
@@ -9,36 +12,41 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(base_dir)
 
 from tools.utils import load_state
+from tools import mlp_policy
+from run import train
+from util import state_desc_to_ob
 
 # Settings
 remote_base = "http://grader.crowdai.org:1729"
-crowdai_token = "[YOUR_CROWD_AI_TOKEN_HERE]"
+with open('./token.txt', 'r') as f:
+    crowdai_token = f.readline()
 
 
 def submit(identifier, policy_fn, seed, iter):
 
-	client = Client(remote_base)
+    client = Client(remote_base)
 
-	# Create environment
-	observation = client.env_create(crowdai_token)
+    # Create environment
+    observation = client.env_create(crowdai_token, env_id="ProstheticsEnv")
 
-	# IMPLEMENTATION OF YOUR CONTROLLER
-	my_controller = train(identifier, policy_fn, 1, 500, seed, save_final=False)
-	load_state(identifier, iter)
+    # IMPLEMENTATION OF YOUR CONTROLLER
+    pi = train(identifier, policy_fn, 1, 500, seed, save_final=False)
+    load_state(identifier, iter)
 
-	while True:
-	    [observation, reward, done, info] = client.env_step(my_controller(observation), True)
-	    if done:
-	        observation = client.env_reset()
-	        if not observation:
-	            break
+    while True:
+        ob = state_desc_to_ob(observation)
+        action = pi.act(False, np.array(ob))[0].tolist()
+        [observation, reward, done, info] = client.env_step(action, True)
+        if done:
+            observation = client.env_reset()
+            if not observation:
+                break
 
-	client.submit()
+    client.submit()
 
 
 def main():
 
-    logger.configure()
     parser = argparse.ArgumentParser(description='Submit.')
     parser.add_argument('--id', type=str, default='origin')
     parser.add_argument('--net', type=int, nargs='+', default=(128, 64))
@@ -50,4 +58,17 @@ def main():
         return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
             hid_layer_sizes=args.net)
 
+    #tf configs
+    ncpu = multiprocessing.cpu_count()
+    if sys.platform == 'darwin': ncpu //= 2
+    config = tf.ConfigProto(allow_soft_placement=True,
+                            intra_op_parallelism_threads=ncpu,
+                            inter_op_parallelism_threads=ncpu)
+    config.gpu_options.allow_growth = True
+    tf.Session(config=config).__enter__()
+
     submit(args.id, policy_fn, args.seed, args.iter)
+
+
+if __name__ == '__main__':
+    main()
