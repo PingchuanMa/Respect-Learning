25e6f2b98752a1fb1d4f8e18b58f62eec3a63c4c
Yunsheng Tian <knoxtian@icloud.com>
2018-09-07 08:07:49 +0000

    limit omp threadsn in training

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,1 +1,1 @@
-mpirun -np 32 python3 run.py --step_per_iter 2048
+OMP_NUM_THREADS=1 mpirun -np 32 python3 run.py --step_per_iter 2048
--- scripts/train_cont.sh
+++ scripts/train_cont.sh
@@ -1,1 +1,1 @@
-mpirun -np 32 python3 run.py --step_per_iter 2048 --cont --iter $1
+OMP_NUM_THREADS=1 mpirun -np 32 python3 run.py --step_per_iter 2048 --cont --iter $1
