e8101970ce9206a8ded8c11a714d4e41e2c49cfa
mapingchuan <mapingchuan@sensetime.com>
2018-09-14 14:42:11 +0800

    merge

--- scripts/train.sh
+++ scripts/train.sh
@@ -38,6 +38,6 @@ step_per_iter=512
 log_dir=../log/.log
 OMP_NUM_THREADS=1
 
-srun -p ${machines} --gres=gpu:${num_gpus} -n=${num_cpus} --job-name=${jobname} --mpi=pmi2 --kill-on-bad-exit=1 \
+srun -p ${machines} --gres=gpu:${num_gpus} -n${num_cpus} --job-name=${jobname} --mpi=pmi2 --kill-on-bad-exit=1 \
 ${python} -u ../run.py --step_per_iter=${step_per_iter} --id=${origin}
        2>&1|tee ${log_dir} &\
