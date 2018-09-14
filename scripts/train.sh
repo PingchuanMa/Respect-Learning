002b2df5eac080272d2a342882a53412f63fb00a
mapingchuan <mapingchuan@sensetime.com>
2018-09-14 14:36:49 +0800

    update scripts

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,1 +1,43 @@
-OMP_NUM_THREADS=1 mpirun -np 8 python3 run.py --step_per_iter 512
+#!/usr/bin/env bash
+
+# Script for training videos in TensorFlow
+
+export LD_LIBRARY_PATH=/mnt/lustre/share/cuda-9.0/lib64/:$LD_LIBRARY_PATH
+if [ ! -d "../log" ]; then
+  mkdir "../log"
+fi
+
+python=/mnt/lustre/DATAshare2/mapingchuan/anaconda3/envs/opensim-rl/bin/python
+
+if [ $1 ]; then
+  identifier=$1
+else
+  identifier=origin
+fi
+
+if [ $2 ]; then
+  num_cpus=$2
+else
+  num_cpus=8
+fi
+
+if [ $3 ]; then
+  num_gpus=$3
+else
+  num_gpus=8
+fi
+
+if [ $4 ]; then
+  machines=$4
+else
+  machines=TITANXP
+fi
+
+jobname=TEST
+step_per_iter=512
+log_dir=../log/.log
+OMP_NUM_THREADS=1
+
+srun -p ${machines} --gres=gpu:${num_gpus} -np=${num_cpus} --job-name=${jobname} --mpi=pmi2 --kill-on-bad-exit=1 \
+${python} -u ../run.py --step_per_iter=${step_per_iter} --id=${origin}
+       2>&1|tee ${log_dir} &\
