8b04f863b06044aeebf04aa7ff5f105c827a7cab
mapingchuan <mapingchuan@sensetime.com>
2018-10-08 22:19:19 +0800

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -33,10 +33,12 @@ else
   machines=TITANXP
 fi
 
+policy=yrh
+
 jobname=TEST
 step_per_iter=512
 log_dir=../log/${identifier}-$( { date +"%H:%M:%S-%h_%d_%y"; } 2>&1 ).log
 
 OMP_NUM_THREADS=1 srun -p ${machines} --gres=gpu:${num_gpus} -n${num_cpus} --cpus-per-task=1 --job-name=${jobname} --mpi=pmi2 --kill-on-bad-exit=1 \
-${python} -u ../run.py --step_per_iter=${step_per_iter} --id=${identifier} --layer_norm --difficulty=1 --reward=7 --fix_target \
+${python} -u ../run.py --step_per_iter=${step_per_iter} --id=${identifier} --layer_norm --difficulty=1 --reward=7 --fix_target --policy=${policy} \
        2>&1|tee ${log_dir} &\
