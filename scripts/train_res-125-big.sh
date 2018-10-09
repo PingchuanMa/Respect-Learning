860972161033b68e3828716ad0d1bd2db6925a59
mapingchuan <mapingchuan@sensetime.com>
2018-10-09 21:12:58 +0800

    update script

--- scripts/train_res-125-big.sh
+++ scripts/train_res-125-big.sh
@@ -20,5 +20,5 @@ step_per_iter=512
 log_dir=../log/${identifier}-$( { date +"%H:%M:%S-%h_%d_%y"; } 2>&1 ).log
 
 OMP_NUM_THREADS=1 srun -p Bigvideo4 -w ${machines} --gres=gpu:${num_gpus} -n${num_cpus} --cpu_bind=threads --ntasks-per-node=${num_cpus} --cpus-per-task=1 --job-name=${jobname} --mpi=pmi2 --kill-on-bad-exit=1 \
-${python} -u ../run.py --step_per_iter=${step_per_iter} --id=${identifier} --layer_norm --difficulty=1 --reward=7 --fix_target --policy=${policy} --net 256 256 256 256 256 256 \
+${python} -u ../run.py --step_per_iter=${step_per_iter} --id=${identifier} --layer_norm --difficulty=1 --reward=8 --fix_target --policy=${policy} --net 256 256 256 256 256 256 \
        2>&1|tee ${log_dir} &\
